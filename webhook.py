84b08ea6c3eb41706725208935a1115eca7097b6
iMaary <m.rodriguesnave@gmail.com>
2021-09-05 20:23:35 -0300

    initial code base flask webhook

---
+++ webhook.py
@@ -0,0 +1,10 @@
+from server import webhook
+import requests
+import json
+
+webhook_url = 'http://127.0.0.1:5000/webhook'
+
+data = {'name': 'cordel',
+        'Channel URL': 'test url'}
+
+r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
